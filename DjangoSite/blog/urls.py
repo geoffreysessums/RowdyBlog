7818c842a24d4a412f4169649e769fc00619177c
geoffreysessums <geoffreysessums@gmail.com>
2019-02-07 10:32:33 -0600

    Added url pattern for share_post feature

--- DjangoSite/blog/urls.py
+++ DjangoSite/blog/urls.py
@@ -9,10 +9,11 @@ urlpatterns = [
     # url pattern for function based view
     # path('', views.post_list, name='post_list'),
 
-    # url pattern for class based view
+    # url patterns for class based view
     path('', views.PostListView.as_view(), name='post_list'),
-
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail,
         name='post_detail'),
+    path('<int:post_id>/share/',
+        views.post_share, name='post_share'),
 ]
