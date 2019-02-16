3861852dccb42b3b228333f6c8aa9195ba361668
geoffreysessums <geoffreysessums@gmail.com>
2019-02-16 14:59:31 -0600

    Added post_share view

--- DjangoSite/blog/views.py
+++ DjangoSite/blog/views.py
@@ -45,7 +45,8 @@ class PostListView(ListView):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
-    sent = False
+    sent = False 
+
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
@@ -53,15 +54,14 @@ def post_share(request, post_id):
             # Form fields passed validation
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(
-                                                  post.get_absolute_url())
-               subject = '{} ({}) recommends you reading "
-               {}"'.format(cd['name'], cd['email'], post.title)
-               message = 'Read "{}" at {}\n\n{}\'s comments:
-                  {}'.format(post.title, post_url, cd['name'], cd['comments'])
-                  send_mail(subject, message, 'admin@myblog.com', 
-                  [cd['to']])
-               sent = True
+                                          post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com',
+ [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form,
-                                                    'sent': sent}))
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+'sent': sent})
