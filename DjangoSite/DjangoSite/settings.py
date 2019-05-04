27e453bbcb30f1256008ab1b1505aa4f3a52792b
geoffreysessums <geoffreysessums@gmail.com>
2019-05-03 21:58:26 -0500

    Added sitemap

--- DjangoSite/DjangoSite/settings.py
+++ DjangoSite/DjangoSite/settings.py
@@ -29,9 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 #    'polls.apps.PollsConfig',
 ]
 
