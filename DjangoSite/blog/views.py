0a8ff93ed826235309ce115fceeb1c6ba7e61870
geoffreysessums <geoffreysessums@gmail.com>
2019-06-11 06:23:17 -0500

    updated views.py

--- DjangoSite/blog/views.py
+++ DjangoSite/blog/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage,\
                                      PageNotAnInteger    
 from django.shortcuts import render, get_object_or_404
@@ -81,25 +80,25 @@ class PostListView(ListView):
    template_name = 'blog/post/list.html'
 
 def post_share(request, post_id):
-   # Retrieve post by id
-   post = get_object_or_404(Post, id=post_id, status='published')
-   sent = False 
-
-   if request.method == 'POST':
-      # Form was submitted
-      form = EmailPostForm(request.POST)
-      if form.is_valid():
-         # Form fields passed validation
-         cd = form.cleaned_data
-         post_url = request.build_absolute_uri(
-                                               post.get_absolute_url())
-         subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
-         message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
-         send_mail(subject, message, 'admin@myblog.com',
-                   [cd['to']])
-         sent = True
-      else:
-         form = EmailPostForm()
-      return render(request, 'blog/post/share.html', {'post': post,
-                                                      'form': form,
-                                                      'sent': sent})
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False 
+ 
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                                          post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com',
+ [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
