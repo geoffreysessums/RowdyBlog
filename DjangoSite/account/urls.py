cbbbf32c10539182115dc9f5b9784ef1d1eb3063
geoffreysessums <geoffreysessums@gmail.com>
2019-07-18 14:17:21 -0500

    Added password reset urls

--- DjangoSite/account/urls.py
+++ DjangoSite/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 from . import views
@@ -16,4 +16,19 @@ urlpatterns = [
     path('password_change/done/',
          auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done'),
+    # reset password urls
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password_reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+    # alternative way to include authentication views
+    # path('', include('django.contrib.auth.urls')),
 ]
