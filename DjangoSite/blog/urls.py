4b987be2c951c0cb8ad61152739ea3a0fc6360d9
geoffreysessums <geoffreysessums@gmail.com>
2019-07-21 10:57:54 -0500

    Added account/to the blog app UrlConf

--- DjangoSite/blog/urls.py
+++ DjangoSite/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'blog'
@@ -21,4 +21,5 @@ urlpatterns = [
          views.post_share, name='post_share'),
     path('search/', views.post_search, name='post_search'),
     path('subscribe/', views.email_list_signup, name='subscribe'),
+    path('account/', include('account.urls')),
 ]
