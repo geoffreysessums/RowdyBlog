9adf924bbc25b7f0f4c78d54e343265138e91cf8
geoffreysessums <geoffreysessums@gmail.com>
2019-07-23 10:49:15 -0500

    Created UserEditForm for user profile

--- DjangoSite/account/forms.py
+++ DjangoSite/account/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
