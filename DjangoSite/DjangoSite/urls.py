845676630a2f2f8bc46b92d1d861c99c03856d39
geoffreysessums <geoffreysessums@gmail.com>
2019-07-23 10:46:34 -0500

    Added settings debug rule to urlpatterns

--- DjangoSite/DjangoSite/urls.py
+++ DjangoSite/DjangoSite/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-
+from django.conf import settings
+from django.conf.urls.static import static
 from blog.sitemaps import PostSitemap
 
 sitemaps = {
@@ -32,3 +33,7 @@ urlpatterns = [
          name='django.contrib.sitemaps.views.sitemap'),
     path('account/', include('account.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
