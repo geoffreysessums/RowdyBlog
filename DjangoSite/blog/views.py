c397591bf76ede26959068aecdfa67db69ae3d10
geoffreysessums <geoffreysessums@gmail.com>
2019-07-25 06:53:49 -0500

    Update added pagination code block (commented out for now)

--- DjangoSite/blog/views.py
+++ DjangoSite/blog/views.py
@@ -1,5 +1,5 @@
-from django.core.paginator import Paginator, EmptyPage,\
-                                     PageNotAnInteger    
+from django.core.paginator import Paginator, EmptyPage, \
+    PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment, Signup
 from django.views.generic import ListView
@@ -20,79 +20,86 @@ from django.conf import settings
 
 import json
 
+
 # Create your views here.
 def post_list(request, tag_slug=None):
-   object_list = Post.published.all()
-   tag = None
+    object_list = Post.published.all()
+    tag = None
 
-   if tag_slug:
-      tag = get_object_or_404(Tag, slug=tag_slug)
-      object_list = object_list.filter(tags__in=[tag])
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
 
-   paginator = Paginator(object_list, 3) # 3 posts in each page
-   page = request.GET.get('page')
-   try:
-      posts = paginator.page(page)
-   except PageNotAnInteger:
-      # If page is not an integer deliver the first page
-      posts = paginator.page(1)
-   except EmptyPage:
-      # If page is out of range deliver last page of results
-      posts = paginator.page(paginator.num_pages)
-   return render(request,
-               'blog/post/list.html',
-               {'page': page,
-                'posts': posts,
-                'tag': tag})
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts,
+                   'tag': tag})
+
 
 def post_detail(request, year, month, day, post):
-   post = get_object_or_404(Post, slug=post,
-                                  status='published',
-                                  publish__year=year,
-                                  publish__month=month,
-                                  publish__day=day)
-   # List of active comments for this post
-   comments = post.comments.filter(active=True)
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
-   new_comment = None 
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
 
-   if request.method == 'POST':
-      # A comment was posted
-      comment_form = CommentForm(data=request.POST)
-      if comment_form.is_valid():
-         # Create Comment object but don't save to database yet
-         new_comment = comment_form.save(commit=False)
-         # Assign the current post to the comment
-         new_comment.post = post
-         # Save the comment to the database
-         new_comment.save()
-   else:
-      comment_form = CommentForm()
-   # List of similar posts
-   post_tags_ids = post.tags.values_list('id', flat=True)
-   similar_posts = Post.published.filter(tags__in=post_tags_ids)\
-                                 .exclude(id=post.id)
-   similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
-                               .order_by('-same_tags','-publish')[:4]
-   return render(request,
-                 'blog/post/detail.html',
-                 {'post': post,
-                  'comments': comments,
-                  'new_comment': new_comment,
-                  'comment_form': comment_form,
-                  'similar_posts': similar_posts})
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
+
 
 class PostListView(ListView):
-   queryset = Post.published.all()
-   context_object_name = 'posts'
-   paginate_by = 3
-   template_name = 'blog/post/list.html'
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
-    sent = False 
- 
+    sent = False
+
     if request.method == 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
@@ -100,11 +107,10 @@ def post_share(request, post_id):
             # Form fields passed validation
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(
-                                          post.get_absolute_url())
+                post.get_absolute_url())
             subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
             message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
-            send_mail(subject, message, 'admin@myblog.com',
- [cd['to']])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
             sent = True
     else:
         form = EmailPostForm()
@@ -112,60 +118,71 @@ def post_share(request, post_id):
                                                     'form': form,
                                                     'sent': sent})
 
-def post_search(request):
-       form = SearchForm()
-       query = None
-       results = []
-       section = 'post_search'
-       if 'query' in request.GET:
-           form = SearchForm(request.GET)
-           if form.is_valid():
-               query = form.cleaned_data['query']
-               search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
-               search_query = SearchQuery(query)
-               results = Post.objects.annotate( rank=SearchRank(search_vector, search_query) ).filter(rank__gte=0.3).order_by('-rank')
-               # Search by similarity is broken
-               #results = Post.objects.annotate(
-               #               similarity=TrigramSimilarity('body', query), ).filter(similarity__gt=0.3).order_by('-similarity')
-       return render(request,
-                     'blog/post/search.html',
-                     {'section': 'post_search',
-                      'form': form,
-                      'query': query,
-                      'results': results})
 
-MAILCHIMP_API_KEY = settings.MAILCHIMP_API_KEY 
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    section = 'post_search'
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.objects.annotate(rank=SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank')
+
+    # paginator = Paginator(results, 3)  # 3 posts in each page
+    # page = request.GET.get('page')
+    # try:
+        # posts = paginator.page(page)
+    # except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        # posts = paginator.page(1)
+    # except EmptyPage:
+        # If page is out of range deliver last page of results
+        # posts = paginator.page(paginator.num_pages)
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'section': 'post_search',
+                   # 'page': page,
+                   # 'posts': posts,
+                   'form': form,
+                   'query': query,
+                   'results': results})
+
+
+MAILCHIMP_API_KEY = settings.MAILCHIMP_API_KEY
 MAILCHIMP_DATA_CENTER = settings.MAILCHIMP_DATA_CENTER
 MAILCHIMP_EMAIL_LIST_ID = settings.MAILCHIMP_EMAIL_LIST_ID
 
 api_url = f'https://{MAILCHIMP_DATA_CENTER}.api.mailchimp.com/3.0'
 members_endpoint = f'{api_url}/lists/{MAILCHIMP_EMAIL_LIST_ID}/members'
 
+
 def subscribe(email):
-   data = {
-      "email_address": email,
-      "status": "subscribed"
-   }
-   r = requests.post(
-      members_endpoint,
-      auth=("", MAILCHIMP_API_KEY),
-      data=json.dumps(data)
-   )
-   return r.status_code, r.json()
+    data = {"email_address": email,
+            "status": "subscribed"}
+    r = requests.post(members_endpoint, auth=("", MAILCHIMP_API_KEY),
+                      data=json.dumps(data))
+    return r.status_code, r.json()
+
 
 def email_list_signup(request):
-   form = EmailSignupForm(request.POST or None)
-   if request.method == "POST":
-      if form.is_valid():
-         email_signup_qs = Signup.objects.filter(email=form.instance.email)
-         if email_signup_qs.exists():
-            messages.info(request, "You are already subscribed")
-         else:
-            messages.success(request, "Thank you for subscribing")
-            subscribe(form.instance.email)
-            form.save()
-   #return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-   return render(request,
-                     'blog/post/subscribe.html',
-                     {'section': 'subscribe',
-                      'form': form})
+    form = EmailSignupForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            email_signup_qs = Signup.objects.filter(email=form.instance.email)
+            if email_signup_qs.exists():
+                messages.info(request, "You are already subscribed")
+            else:
+                messages.success(request, "Thank you for subscribing")
+                subscribe(form.instance.email)
+                form.save()
+    # return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    return render(request,
+                  'blog/post/subscribe.html',
+                  {'section': 'subscribe',
+                   'form': form})
